28025dc95c6571fc5dc5233f15ab7443d6940cdf
ST10257002 <131689755+ST10257002@users.noreply.github.com>
2023-06-19 19:22:21 +0200

    Create test class for the interpreter

---
+++ test/app/InterpretTest.java
@@ -0,0 +1,60 @@
+
+package app;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class InterpretTest {
+    
+    public InterpretTest() {
+    }
+
+    @Test
+    public void testUpdateTable() {
+    }
+
+    @Test
+    public void testUpdateLogFile() {
+    }
+
+    @Test
+    public void testGetTotal() {
+    }
+
+    @Test
+    public void testGetWords() {
+    }
+
+    @Test
+    public void testGetWordsUnique() {
+    }
+
+    @Test
+    public void testGetCharacters() {
+    }
+
+    @Test
+    public void testGetSpace() {
+    }
+
+    @Test
+    public void testGetSymbols() {
+    }
+
+    @Test
+    public void testGetLetters() {
+    }
+
+    @Test
+    public void testGetLettersUpperCase() {
+    }
+
+    @Test
+    public void testGetLettersLowerCase() {
+    }
+
+    @Test
+    public void testGetDigits() {
+    }
+    
+}
